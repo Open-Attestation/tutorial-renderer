c0636c0b94e40ccb87a1dc25bed1b0e43a0cf1c0
Nebulis <nebounet@gmail.com>
2021-03-19 15:56:11 +0800

    feat: supports PDF

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { FramedDocumentRenderer, PdfRenderer, utils } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+ReactDOM.render(
+  <FramedDocumentRenderer
+    templateRegistry={registry}
+    attachmentToComponent={attachment => {
+      if (utils.getAttachmentMimeType(attachment) === "application/pdf") {
+        // eslint-disable-next-line react/display-name
+        return () => <PdfRenderer attachment={attachment} />;
+      }
+      return null;
+    }}
+  />,
+  document.getElementById("root")
+);
